eb0ebb57220e5372db5b420566e2c4abcae723e6
Thierry BRU <thierry-bru@laposte.net>
2024-04-04 16:03:35 +0200

    classe quizz creee sans association

--- src/model/Quizz.php
+++ src/model/Quizz.php
@@ -1,5 +1,12 @@
 <?php
 namespace app\quizz\model;
 class Quizz{
-
+    private string $_title;
+    public function __construct(string $title = 'No title choosen') {
+        $this->_title = $title;
+    }
+    public function getTitle():string
+    {
+        return $this->_title;
+    }
 }
