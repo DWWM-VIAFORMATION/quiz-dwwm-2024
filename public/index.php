7b3a7b05383342e21e4537e731eb491276716b15
Thierry BRU <thierry-bru@laposte.net>
2024-04-17 08:46:51 +0200

    base pour fiche pratique - mettre en place le routage

--- public/index.php
+++ public/index.php
@@ -1,19 +1,4 @@
 <?php
 declare(strict_types=1);
-use app\quizz\router\HttpRequest;
-use app\quizz\router\Router;
-
 require_once dirname(__DIR__) .'/vendor/autoload.php';
 
-try
-	{
-		$httpRequest = new HttpRequest();
-        $router = new Router();
-        $httpRequest->setRoute($router->findRoute($httpRequest));
-        $httpRequest->getRoute()->run();
-	}
-	catch(Exception $e)
-	{
-        echo "Une erreur s'est produite";
-        echo $e->getMessage();
-	}
